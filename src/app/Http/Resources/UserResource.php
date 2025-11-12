21e9b6a83315d159ce2cdba5868ad4f8036b5aa6
Mochamad Sohibul Iman <iman@imansohibul.my.id>
2025-11-12 09:42:37 +0700

    Remove unused code

--- src/app/Http/Resources/UserResource.php
+++ src/app/Http/Resources/UserResource.php
@@ -21,16 +21,4 @@ class UserResource extends JsonResource
             'created_at' => $this->created_at->toIso8601String(),
         ];
     }
-
-    /**
-     * Customize the outgoing response for the resource.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Http\Response  $response
-     * @return void
-     */
-    public function withResponse($request, $response): void
-    {
-        $response->setStatusCode(201);
-    }
 }
